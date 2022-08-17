9d46494b1f45a6c1d9c46d5cd130fd2e458b0f73
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-08-17 14:44:04 -0700

    implement TranslatedNonWhitespacesGroupPattern

--- tests/unit/test_gp.py
+++ tests/unit/test_gp.py
@@ -693,3 +693,42 @@ class TestTranslatedNonWhitespacesPattern:
         recommended_pat_obj = node.recommend(other)
         recommended_pat = recommended_pat_obj.pattern
         assert recommended_pat == expected_pattern
+
+
+class TestTranslatedNonWhitespacesGroupPattern:
+    """Test class for TranslatedNonWhitespacesGroupPattern."""
+
+    @pytest.mark.parametrize(
+        "data,expected_pattern",
+        [
+            ('', ''),
+            ('aa', r'\S+( +\S+)*'),
+            ('1', r'\S+( +\S+)*'),
+            ('+', r'\S+( +\S+)*'),
+            ('a', r'\S+( +\S+)*'),
+        ]
+    )
+    def test_non_whitespace_pattern(self, data, expected_pattern):
+        node = TranslatedNonWhitespacesGroupPattern(data)
+        pattern = node.pattern
+        assert pattern == expected_pattern
+
+    @pytest.mark.parametrize(
+        "data,other,expected_pattern",
+        [
+            ('-', TranslatedLetterPattern('a'), r'\S+( +\S+)*'),
+            ('.', TranslatedLettersPattern('ab'), r'\S+( +\S+)*'),
+            ('+', TranslatedDigitPattern('1'), r'\S+( +\S+)*'),
+            ('*', TranslatedDigitsPattern('42'), r'\S+( +\S+)*'),
+            ('a', TranslatedAlphabetNumericPattern('4'), r'\S+( +\S+)*'),
+            ('}', TranslatedWordPattern('4'), r'\S+( +\S+)*'),
+            ('=', TranslatedWordsPattern('4 5'), r'\S+( +\S+)*'),
+            ('@', TranslatedNonWhitespacesPattern('4'), r'\S+( +\S+)*'),
+            ('#', TranslatedNonWhitespacesGroupPattern('4'), r'\S+( +\S+)*'),
+        ]
+    )
+    def test_recommend_pattern(self, data, other, expected_pattern):
+        node = TranslatedNonWhitespacesGroupPattern(data)
+        recommended_pat_obj = node.recommend(other)
+        recommended_pat = recommended_pat_obj.pattern
+        assert recommended_pat == expected_pattern
