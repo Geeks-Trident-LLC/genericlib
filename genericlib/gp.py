0ed26d7e46b50b22879a7be1f9625f4a1f74dd33
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-08-18 13:21:19 -0700

    rework gp.TranslatedSymbolPattern

--- genericlib/gp.py
+++ genericlib/gp.py
@@ -525,44 +525,42 @@ class TranslatedSymbolPattern(TranslatedPattern):
         super().__init__(data, *other, name=TEXT.SYMBOL,
                          defined_pattern=PATTERN.SYMBOL)
 
+    def is_subset_of(self, other):
+        chk = other.is_symbol() or other.is_graph()
+        chk = chk or other.is_symbols() or other.is_symbols_group()
+        chk = chk or other.is_mixed_word() or other.is_mixed_words()
+        chk = chk or other.is_non_whitespace() or other.is_non_whitespaces()
+        chk = chk or other.is_non_whitespaces_group()
+
+        return chk
+
+    def is_superset_of(self, other):
+        return False
+
     def recommend(self, other):
 
-        is_subset_pat = other.is_symbol()
-        is_subset_pat |= other.is_graph()
-        is_subset_pat |= other.is_symbols()
-        is_subset_pat |= other.is_symbols_group()
-        is_subset_pat |= other.is_mixed_word()
-        is_subset_pat |= other.is_mixed_words()
-        is_subset_pat |= other.is_non_whitespace()
-        is_subset_pat |= other.is_non_whitespaces()
-        is_subset_pat |= other.is_non_whitespaces_group()
-
-        is_new_pat_case1 = other.is_letter() or other.is_digit()
-        is_new_pat_case1 |= other.is_alphabet_numeric()
-
-        is_new_pat_case2 = other.is_letters() or other.is_digits()
-        is_new_pat_case2 |= other.is_number() or other.is_mixed_number()
-        is_new_pat_case2 |= other.is_word()
-
-        is_new_pat_case3 = other.is_words()
-
-        if is_subset_pat:
-            new_instance = other(other.data, other.get_reference_data(self))
-            return new_instance
-        elif is_new_pat_case1:
-            new_instance = TranslatedGraphPattern(self.data)
-            return new_instance
-        elif is_new_pat_case2:
-            new_instance = TranslatedMixedWordPattern(self.data, other.data)
-            return new_instance
-        elif is_new_pat_case3:
-            new_instance = TranslatedMixedWordsPattern(self.data, other.data)
-            return new_instance
+        if self.is_subset_of(other) or self.is_superset_of(other):
+            if self.is_subset_of(other):
+                return self.get_new_subset(other)
+            else:
+                return self.get_new_superset(other)
         else:
-            cls_name = Misc.get_instance_class_name(self)
-            fmt = 'Need to implement this case (%r, %r) for %s'
-            err_msg = fmt % (self.data, other.data, cls_name)
-            raise Exception(err_msg)
+            case1 = other.is_letter() or other.is_digit() or other.is_alphabet_numeric()
+
+            case2 = other.is_letters() or other.is_digits()
+            case2 = case2 or other.is_number() or other.is_mixed_number()
+            case2 = case2 or other.is_word()
+
+            case3 = other.is_words()
+
+            if case1:
+                return TranslatedGraphPattern(self.data)
+            elif case2:
+                return TranslatedMixedWordPattern(self.data, other.data)
+            elif case3:
+                return TranslatedMixedWordsPattern(self.data, other.data)
+            else:
+                self.raise_recommend_exception(other)
 
 
 class TranslatedSymbolsPattern(TranslatedPattern):
