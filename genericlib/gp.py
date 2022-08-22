9406829a48806cd1c31abb28d7aa136916a7475f
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-08-22 14:55:51 -0700

    add DiffLinePattern.is_leading property

--- genericlib/gp.py
+++ genericlib/gp.py
@@ -1296,6 +1296,11 @@ class DiffLinePattern:
         chk = all(line.endswith(STRING.SPACE_CHAR) for line in self.raw_lines)
         return chk
 
+    @property
+    def is_leading(self):
+        chk = any(line.startswith(STRING.SPACE_CHAR) for line in self.raw_lines)
+        return chk
+
     def reset(self):
         self.lines.clear()
         self._pattern = ''
