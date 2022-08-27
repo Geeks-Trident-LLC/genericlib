21b77b4436f470ea7be4f393afa9d595b3432a0f
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-08-26 17:25:06 -0700

    update genericlib to version 0.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='genericlib',
-    version='0.3.1',
+    version='0.3.2',
     license='Geeks Trident License',
     license_files=['LICENSE'],
     description='Describe-Get-System',
