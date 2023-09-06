02c479daecb12061fae1b64707082493657d41b1
Tuyen M. Duong <tuyen@geekstrident.com>
2023-09-05 18:19:02 -0700

    add more pattern keywords for ref_pattern.yaml and upgrade to version 0.4.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='genericlib',
-    version='0.4.3',
+    version='0.4.4',
     license='Geeks Trident License',
     license_files=['LICENSE'],
     description='Generic Library',
