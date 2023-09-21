4c889f7cd7d5e81979519b3a01ecd5908cd15a43
Tuyen M. Duong <tuyen@geekstrident.com>
2023-09-21 00:49:21 -0700

    update genericlib to version 0.5.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='genericlib',
-    version='0.5.0',
+    version='0.5.1',
     license='Geeks Trident License',
     license_files=['LICENSE'],
     description='Generic Library',
