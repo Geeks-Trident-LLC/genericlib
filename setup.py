17be876678369259e1a5a27afc8463df40006876
Tuyen M. Duong <tuyen@geekstrident.com>
2025-12-10 09:42:23 -0800

    Update regexbuilder and textfsmgenerator dependencies in requirements.txt. (Note: upcoming migration will remove these packages from genericlib.)

--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ setup(
     maintainer_email='tuyen@geekstrident.com',
     install_requires=[
         'pyyaml',
+        'regexbuilder',
+        'textfsmgenerator'
     ],
     url='https://github.com/Geeks-Trident-LLC/genericlib',
     packages=find_packages(
