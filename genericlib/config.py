326f2fbd50ee49b23815c4b54cae32ca033a36bc
Tuyen M. Duong <tuyen@geekstrident.com>
2025-12-11 16:35:44 -0800

    chore: update setup.py for compliance with Python packaging standards

--- genericlib/config.py
+++ genericlib/config.py
@@ -1,4 +1,4 @@
-__version__ = '0.6.0'
+__version__ = '0.6.0a0'
 version = __version__
 
 __all__ = [
--- setup.py
+++ setup.py
@@ -1,64 +1,73 @@
 from setuptools import setup, find_packages
 
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
-    name='genericlib',
-    version='0.6.0',
-    license='BSD-3-Clause',
-    license_files=['LICENSE'],
-    description='Generic Library',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Tuyen Mathew Duong',
-    author_email='tuyen@geekstrident.com',
-    maintainer='Tuyen Mathew Duong',
-    maintainer_email='tuyen@geekstrident.com',
+    name="genericlib",
+    version="0.6.0a0",  # alpha versioning
+    license="BSD-3-Clause",
+    license_files=["LICENSE"],
+    description="The Generic Python Library accelerates development with "
+                "reusable utilities, supporting regexgenerator and "
+                "textfsmgenerator while streamlining workflows, "
+                "reducing redundancy, and enabling efficient, adaptable, "
+                "maintainable applications.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Tuyen Mathew Duong",
+    author_email="tuyen@geekstrident.com",
+    maintainer="Tuyen Mathew Duong",
+    maintainer_email="tuyen@geekstrident.com",
     install_requires=[
-        'pyyaml',
+        "pyyaml",
     ],
-    url='https://github.com/Geeks-Trident-LLC/genericlib',
+    url="https://github.com/Geeks-Trident-LLC/genericlib",
     packages=find_packages(
         exclude=(
-            'tests*', 'testing*', 'examples*',
-            'build*', 'dist*', 'docs*', 'venv*'
+            "tests*", "testing*", "examples*",
+            "build*", "dist*", "docs*", "venv*"
         )
     ),
+    project_urls={
+        "Documentation": "https://github.com/Geeks-Trident-LLC/genericlib/wiki",
+        "Source": "https://github.com/Geeks-Trident-LLC/genericlib",
+        "Tracker": "https://github.com/Geeks-Trident-LLC/genericlib/issues",
+    },
     include_package_data=True,
-    test_suite='tests',
+    test_suite="tests",
     classifiers=[
         # development status
-        'Development Status :: 2 - Pre-Alpha',
+        "Development Status :: 3 - Alpha",
         # natural language
-        'Natural Language :: English',
+        "Natural Language :: English",
         # intended audience
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Manufacturing',
-        'Intended Audience :: Other Audience',
-        'Intended Audience :: Science/Research',
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Manufacturing",
+        "Intended Audience :: Other Audience",
+        "Intended Audience :: Science/Research",
         # operating system
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: Microsoft :: Windows',
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
         # license
-        "License :: OSI Approved :: BSD License",  # BSD-3-Clause classifier
+        "License :: OSI Approved :: BSD License",
         # programming language
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         # topic
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Software Development :: Code Generators',
-        'Topic :: Software Development :: Quality Assurance',
-        'Topic :: Software Development :: Testing',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Text Processing',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Text Processing",
     ],
 )
