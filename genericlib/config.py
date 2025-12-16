957215c6e5575a338a2f91f1de63821aa2e775fd
Tuyen M. Duong <tuyen@geekstrident.com>
2025-12-15 19:16:29 -0800

    docs(config): improve docstrings for all classes and methods or functions

--- genericlib/config.py
+++ genericlib/config.py
@@ -1,3 +1,10 @@
+"""
+genericlib metadata and attributes.
+
+This module centralizes versioning and edition information for the
+`genericlib` package.
+"""
+
 __version__ = '0.6.0a0'
 version = __version__
 
